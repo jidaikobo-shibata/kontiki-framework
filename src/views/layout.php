896e655779cccd9d7a04274eb8f6b6b4f6c4a027
jidaikobo-shibata <shibata@jidaikobo.com>
2025-02-19 22:14:39 +0900

    add favicon

--- src/views/layout.php
+++ src/views/layout.php
@@ -3,6 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php
+    $faviconPath = env('ADMIN_FAVICON_PATH', '');
+    if (!empty($faviconPath)) :
+        echo '  <link rel="shortcut icon" href="' . $faviconPath . '">';
+    endif;
+  ?>
 
   <!-- AdminLTE CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
