10a9e778fca0483a141dffb7a0d6c21a3d6aa602
jidaikobo-shibata <shibata@jidaikobo.com>
2025-02-21 20:02:04 +0900

    dashboard control

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -7,21 +7,19 @@ use Jidaikobo\Kontiki\Services\GetRoutesService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
-use Slim\Routing\RouteCollector;
 use Slim\Routing\RouteCollectorProxy;
-use Slim\Routing\RouteParser;
 use Slim\Views\PhpRenderer;
 
 class DashboardController
 {
     private PhpRenderer $view;
-    private RouteParser $routeParser;
-    private RouteCollector $routeCollector;
+    private array $routes;
 
     public function __construct(PhpRenderer $view, GetRoutesService $getRoutesService)
     {
         $this->view = $view;
         $this->view->setAttributes(['sidebarItems' => $getRoutesService->getSidebar()]);
+        $this->routes = $getRoutesService->getLinks();
     }
 
     public static function registerRoutes(App $app): void
@@ -37,12 +35,47 @@ class DashboardController
 
     public function dashboard(Request $request, Response $response): Response
     {
+        $namedRoutes = array_filter($this->routes, function ($item) {
+            return isset($item['name']) &&
+                (
+                    strpos($item['name'], 'index') !== false ||
+                    strpos($item['name'], 'create') !== false
+                );
+        });
+
+        $postTypes = [];
+        foreach ($namedRoutes as $namedRoute)
+        {
+            $postType = str_replace(['_index', '_create'], '', $namedRoute['name']);
+            if (strpos($namedRoute['name'], $postType) === false) continue;
+            $postTypes[$postType][] = $namedRoute;
+        }
+
+        $html = '';
+        foreach ($postTypes as $postTypeName => $postTypeVals)
+        {
+            $html.= '<div class="card"><div class="card-header">';
+            $html.= '<h2 class="card-title">' . __($postTypeName) . '</h3>';
+            $html.= '</div><div class="card-body"><ul>';
+            foreach ($postTypeVals as $val)
+            {
+                $langLabel = preg_replace('/^[^_]+/', 'x', $val['name']);
+                $html.= '<li>';
+                $html.= '<a href="' . env('BASEPATH') . $val['path'] . '">';
+                $html.= __($langLabel, ':name index', ['name' => __($postTypeName)]);
+                $html.= '</a>';
+                $html.= '</li>';
+            }
+            $html.= '</ul></div></div>';
+        }
+
+
         return $this->view->render(
             $response,
             'layout.php',
             [
                 'pageTitle' => __('management_portal', 'Management Portal'),
-                'content' => '',
+                'content' => $html,
             ]
         );
         ;
