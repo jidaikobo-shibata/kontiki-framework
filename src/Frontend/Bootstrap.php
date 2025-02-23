9fb80b7696c4fdabbfde604cf7eddc78c6560451
jidaikobo-shibata <shibata@jidaikobo.com>
2025-02-23 15:13:39 +0900

    add comments

--- src/Frontend/Bootstrap.php
+++ src/Frontend/Bootstrap.php
@@ -8,7 +8,10 @@ class Bootstrap
 {
     public static function init(string $env = 'production')
     {
+        // prepare timer, log and functions
         BaseBootstrap::init($env, true);
+
+        // prepare frontend functions
         require __DIR__ . '/../functions/frontend.php';
     }
 }
