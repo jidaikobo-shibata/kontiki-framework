2c8729450082a2e302ddfd00c5c718a5e932f163
jidaikobo-shibata <shibata@jidaikobo.com>
2025-03-17 22:49:47 +0900

    fix DI

--- src/functions/frontend.php
+++ src/functions/frontend.php
@@ -4,6 +4,7 @@ use Jidaikobo\Kontiki\Core\Database;
 use Jidaikobo\Kontiki\Core\Auth;
 use Jidaikobo\Kontiki\Models\UserModel;
 use Jidaikobo\Kontiki\Models\PostModel;
+use Jidaikobo\Kontiki\Validation\BaseValidator;
 
 if (!function_exists('getIndex')) {
     /**
@@ -15,13 +16,11 @@ if (!function_exists('getIndex')) {
     {
         $app = Jidaikobo\Kontiki\Bootstrap::init($env);
         $container = $app->getContainer();
-        $userModel = new UserModel(
-            $container->get(Database::class)
-        );
         $model = new PostModel(
             $container->get(Database::class),
             $container->get(Auth::class),
-            $userModel
+            $container->get(UserModel::class),
+            $container->get(BaseValidator::class)
         );
         $retval = [];
         $retval['body'] = $model->getIndexData('published', $args);
@@ -40,13 +39,64 @@ if (!function_exists('getData')) {
     {
         $app = Jidaikobo\Kontiki\Bootstrap::init($env);
         $container = $app->getContainer();
-        $userModel = new UserModel(
-            $container->get(Database::class)
-        );
         $model = new PostModel(
             $container->get(Database::class),
             $container->get(Auth::class),
-            $userModel
+            $container->get(UserModel::class),
+            $container->get(BaseValidator::class)
+        );
+        $slug = $args['slug'] ?? '';
+        $retval = [];
+        $retval['body'] = $model->getByFieldWithCondtioned('slug', $slug, 'published');
+        return $retval;
+    }
+}
+<?php
+
+use Jidaikobo\Kontiki\Core\Database;
+use Jidaikobo\Kontiki\Core\Auth;
+use Jidaikobo\Kontiki\Models\UserModel;
+use Jidaikobo\Kontiki\Models\PostModel;
+use Jidaikobo\Kontiki\Validation\BaseValidator;
+
+if (!function_exists('getIndex')) {
+    /**
+     * @param array $args Configuration for the request.
+     * @param string $env environment.
+     * @return array
+     */
+    function getIndex(array $args, string $env = 'production'): array
+    {
+        $app = Jidaikobo\Kontiki\Bootstrap::init($env);
+        $container = $app->getContainer();
+        $model = new PostModel(
+            $container->get(Database::class),
+            $container->get(Auth::class),
+            $container->get(UserModel::class),
+            $container->get(BaseValidator::class)
+        );
+        $retval = [];
+        $retval['body'] = $model->getIndexData('published', $args);
+        $retval['pagination'] = $model->getPagination();
+        return $retval;
+    }
+}
+
+if (!function_exists('getData')) {
+    /**
+     * @param array $args Configuration for the request.
+     * @param string $env environment.
+     * @return array
+     */
+    function getData(array $args, string $env = 'production'): array
+    {
+        $app = Jidaikobo\Kontiki\Bootstrap::init($env);
+        $container = $app->getContainer();
+        $model = new PostModel(
+            $container->get(Database::class),
+            $container->get(Auth::class),
+            $container->get(UserModel::class),
+            $container->get(BaseValidator::class)
         );
         $slug = $args['slug'] ?? '';
         $retval = [];
